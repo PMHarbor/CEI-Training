8c6f6a0087f5b3909e5aa33a5b9851d833de0aa1
Matthew Flebbe <matt.flebbe@pmharbor.com>
2016-10-23 11:27:47 -0400

    Ready for presentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 window.onload = function(){
 
-            //step 1: get the DOM object of the iframe.
+            // Introduction Video
 
             var iframe = document.getElementById('test_iframe');
-            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/IenWr92F-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
+            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/JocLyJgy-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
 
 
             var iframedoc = iframe.document;
@@ -24,12 +24,64 @@ window.onload = function(){
              }
         }
 
-        function injectHTML2(){
+        // First Video
 
-            //step 1: get the DOM object of the iframe.
+        function injectHTML1(){
 
             var iframe = document.getElementById('test_iframe');
-            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/n1HLaQRO-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
+            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/TnfSheNM-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
+
+
+            var iframedoc = iframe.document;
+                if (iframe.contentDocument)
+                    iframedoc = iframe.contentDocument;
+                else if (iframe.contentWindow)
+                    iframedoc = iframe.contentWindow.document;
+
+             if (iframedoc){
+                 // Put the content in the iframe
+                 iframedoc.open();
+                 iframedoc.writeln(html_string);
+                 iframedoc.close();
+             } else {
+                //just in case of browsers that don't support the above 3 properties.
+                //fortunately we don't come across such case so far.
+                alert('Cannot inject dynamic contents into iframe.');
+             }
+        }
+
+        // Second Video
+
+        function injectHTML2(){
+
+            var iframe = document.getElementById('test_iframe');
+            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/kXpH1zmm-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
+
+
+            var iframedoc = iframe.document;
+                if (iframe.contentDocument)
+                    iframedoc = iframe.contentDocument;
+                else if (iframe.contentWindow)
+                    iframedoc = iframe.contentWindow.document;
+
+             if (iframedoc){
+                 // Put the content in the iframe
+                 iframedoc.open();
+                 iframedoc.writeln(html_string);
+                 iframedoc.close();
+             } else {
+                //just in case of browsers that don't support the above 3 properties.
+                //fortunately we don't come across such case so far.
+                alert('Cannot inject dynamic contents into iframe.');
+             }
+        }
+
+        // Third Video
+
+        function injectHTML3(){
+
+            var iframe = document.getElementById('test_iframe');
+            var html_string = '<html><head></head><body><div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="//content.jwplatform.com/players/2qyP34Ws-H6ePhpfv.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div></body></html>';
 
 
             var iframedoc = iframe.document;
